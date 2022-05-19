259bc07bc991855dccadf49ee719250fc680b92a
AJ <ia.jordaan1003@gmail.com>
2022-05-19 15:02:45 +0200

    Add project files.

---
+++ CSharpMasterclass/Program.cs
@@ -0,0 +1,26 @@
+﻿using MasterclassLibrary;
+
+/* ========================< Assignment 1 >======================== */
+string stringForFloat = "0,85"; // datatype should be float
+string stringForInt = "12345"; // datatype should be int
+
+var floatOutput = float.Parse(stringForFloat);
+var intOutput = int.Parse(stringForInt);
+
+Console.WriteLine($"The float value is {floatOutput}");
+Console.WriteLine($"The int value is {intOutput}");
+Console.WriteLine();
+
+/* ------------------------< Challenge 1 >------------------------ */
+var stringOutput = new StringChallenge1();
+string userInput;
+
+Console.WriteLine("Please enter your name and press 'Enter'.");
+Console.Write("-> ");
+userInput = Console.ReadLine();
+
+var result = stringOutput.PrintDifferentStringOuputs(userInput);
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
---
+++ MasterclassClassLibrary/Section2/StringChallenge1.cs
@@ -0,0 +1,16 @@
+﻿namespace MasterclassLibrary
+{
+    public class StringChallenge1
+    {
+        public List<string> PrintDifferentStringOuputs(string input)
+        {
+            var output = new List<string>();
+
+            output.Add(input.ToUpper());
+            output.Add(input.ToLower());
+            output.Add(input.Substring(input.IndexOf(' ')).Trim());
+
+            return output;
+        }
+    }
+}
